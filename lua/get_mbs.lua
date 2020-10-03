0e525921d2b0c4ac8cb8d7804ac414d9551223be
Max <max@debian-fx>
2020-10-03 23:03:00 +0200

    Added support for functions in key_table

--- lua/get_mbs.lua
+++ lua/get_mbs.lua
@@ -10,6 +10,9 @@ get_mbs = function(callback, key_table, max_i, i)
 		return key_code, false
 	end
 	local key_resolved = key_table[key_code]
+	if type(key_resolved) == "function" then
+		key_resolved = key_resolved(callback, key_code)
+	end
 	if type(key_resolved) == "table" then
 		-- we're in a multibyte sequence, get more characters recursively(with maximum limit)
 		return get_mbs(callback, key_resolved, max_i, i+1)
